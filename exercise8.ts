2ab0fd513270d896bbd73d24f55fa8409f11973f
bmQ88rk4Ry?3c?iC <t3doch00@students.oamk.fi>
2024-02-01 14:49:11 +0200

    Fixed

--- exercise8.ts
+++ exercise8.ts
@@ -1,25 +1,5 @@
 /* Take his dataset of earth meteorite landings from NASA https://data.nasa.gov/resource/y77d-th95.json . Create an application which loads the dataset as a JSON file. You can load JSON file to node application with var someObject = require('./somefile.json'). Create a function, which prints the names of all meteorites.
-
-{
-        "name": "Aachen",
-        "id": "1",
-        "nametype": "Valid",
-        "recclass": "L5",
-        "mass": "21",
-        "fall": "Fell",
-        "year": "1880-01-01T00:00:00.000",
-        "reclat": "50.775000",
-        "reclong": "6.083330",
-        "geolocation": {
-            "type": "Point",
-            "coordinates": [
-                6.08333,
-                50.775
-            ]
-        }
-    },
 */
-
 const meteoritesDataset = require('./meteorites.json');
 interface Meteorite{
     name: string;
